68a80eaedc6ffdabacb7d632491a96ca8beda5f8
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-08-30 15:32:17 -0500

    commit tree 22989deafa68a3e542c1ee8243f7d98594affb72

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,21 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+
+  const projItems = projects.map(project => {
+      return (<div key={project.id}>
+          <h2>{project.name}</h2>
+          <p>{project.about}</p>
+          {project.technologies.map(tech =>
+            <button>{tech}</button>)}
+        </div>)
+  })
+
+f
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projItems}</div>
     </div>
   );
 }
